773981bbd945c87f34ecf113c4ff33bc8da4bcc6
Okai Yeboah <kingkloy16@gmail.com>
2020-08-22 20:35:54 +0000

    update assets images

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:w2schools/Screens/TestButton/TestButton.dart';
+import 'package:w2schools/Screens/dashboard_screen.dart';
 import 'package:w2schools/app_state/walkthroughState.dart';
 
 void main() {
   runApp(
-    
-        MultiProvider(
-      
-      providers: [
+    MultiProvider(providers: [
+      ChangeNotifierProvider<WalkThroughState>(
+          create: (context) => WalkThroughState()),
 
-        ChangeNotifierProvider<WalkThroughState>(create: (context) =>  WalkThroughState()),
-
-     // Put The New State Class you create
-    //  ChangeNotifierProvider<YourNewStateClass>(create: (context) =>  YourNewStateClass()),                
-      ],
-    child: MyApp()),
-    );
+      // Put The New State Class you create
+      //  ChangeNotifierProvider<YourNewStateClass>(create: (context) =>  YourNewStateClass()),
+    ], child: MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
+  static Map<int, Color> color = {
+    50: Color.fromRGBO(252, 179, 13, .1),
+    100: Color.fromRGBO(252, 179, 13, .2),
+    200: Color.fromRGBO(252, 179, 13, .3),
+    300: Color.fromRGBO(252, 179, 13, .4),
+    400: Color.fromRGBO(252, 179, 13, .5),
+    500: Color.fromRGBO(252, 179, 13, .6),
+    600: Color.fromRGBO(252, 179, 13, .7),
+    700: Color.fromRGBO(252, 179, 13, .8),
+    800: Color.fromRGBO(252, 179, 13, .9),
+    900: Color.fromRGBO(252, 179, 13, 1),
+  };
 
-  
-static Map<int, Color> color = {
-50: Color.fromRGBO(252, 179, 13, .1),
-100: Color.fromRGBO(252, 179, 13, .2),
-200: Color.fromRGBO(252, 179, 13, .3),
-300: Color.fromRGBO(252, 179, 13, .4),
-400: Color.fromRGBO(252, 179, 13, .5),
-500: Color.fromRGBO(252, 179, 13, .6),
-600: Color.fromRGBO(252, 179, 13, .7),
-700: Color.fromRGBO(252, 179, 13, .8),
-800: Color.fromRGBO(252, 179, 13, .9),
-900: Color.fromRGBO(252, 179, 13, 1),
-};
-
-final MaterialColor colorCustom = MaterialColor(0xffFCB30D, color);
-  
+  final MaterialColor colorCustom = MaterialColor(0xffFCB30D, color);
 
   @override
   Widget build(BuildContext context) {
@@ -47,13 +40,9 @@ final MaterialColor colorCustom = MaterialColor(0xffFCB30D, color);
         primarySwatch: colorCustom,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: 
-      // Put Your Entry Widget here
-      TestButton(),
+      home:
+          // Put Your Entry Widget here
+          DashboardScreen(),
     );
   }
 }
-
-
-
-
